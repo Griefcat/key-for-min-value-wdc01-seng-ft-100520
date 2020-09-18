ff12f22210c90c57a4654a2b28ce0a90df9586af
Anne Nardolilli <anardolilli@gmail.com>
2020-09-18 00:15:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@
 
 
 def key_for_min_value(name_hash)
-  key = ""
+  min_key = nil
+  min_value = nil
   name_hash.each do |key, value|
-    if :value1 < :value2 
-    key = name_hash[:value]
+    if value < min_value
+      min_value = value 
+    min_key = key 
 end
 end
-key  
+min_key  
 end
